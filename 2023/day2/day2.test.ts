6ed78d18252569f06af0b29490268d7a546df10b
Enzo Testa <enzotesta@hotmail.fr>
2023-12-03 17:21:00 +0100

    ⭐️x6

--- 2023/day2/day2.test.ts
+++ 2023/day2/day2.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { resolveDemo1, resolvePart1 } from "./day2";
+import { resolveDemo1, resolveDemo2, resolvePart1, resolvePart2 } from "./day2";
 
 describe("Day 2", () => {
   it("should solve the demo file", async () => {
@@ -8,5 +8,13 @@ describe("Day 2", () => {
 
   it("should solve the first part", async () => {
     expect(await resolvePart1()).to.equal(2076);
+  });
+
+  it("should solve the second demo", async () => {
+    expect(await resolveDemo2()).to.equal(2286);
+  });
+
+  it("should solve the first part", async () => {
+    expect(await resolvePart2()).to.equal(70950);
   });
 });
--- 2023/day2/day2.ts
+++ 2023/day2/day2.ts
@@ -28,6 +28,23 @@ async function resolve(filename: string) {
   return sum(indexesOfPossibleGames);
 }
 
+async function resolve2(filename: string) {
+  const lines = await parseLines(filename, __dirname);
+  const games = lines.map(parseLine);
+
+  return sum(games.map(computeGamePower));
+}
+
+function computeGamePower(sets: Array<Record<string, number>>) {
+  const maxes = { green: 0, red: 0, blue: 0 };
+  sets.forEach(set => {
+    maxes.green = Math.max(set.green || 0, maxes.green);
+    maxes.red = Math.max(set.red || 0, maxes.red);
+    maxes.blue = Math.max(set.blue || 0, maxes.blue);
+  });
+  return Object.values(maxes).reduce((acc, val) => acc * val);
+}
+
 function isGamePossible(sets: Array<Record<string, number>>) {
   return ! sets.some(set => set.blue > LIMITS.blue || set.red > LIMITS.red || set.green > LIMITS.green);
 }
@@ -38,4 +55,12 @@ export async function resolveDemo1() {
 
 export async function resolvePart1() {
   return resolve("day2.input.txt");
+}
+
+export async function resolveDemo2() {
+  return resolve2("day2.input.demo.txt");
+}
+
+export async function resolvePart2() {
+  return resolve2("day2.input.txt");
 }
