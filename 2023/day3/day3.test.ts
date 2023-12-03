97a7c7a7f200cfb12fb383ec675547bd2d330fb3
Enzo Testa <enzotesta@hotmail.fr>
2023-12-03 16:02:39 +0100

    ⭐️ x4

--- 2023/day3/day3.test.ts
+++ 2023/day3/day3.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { resolvePart1 } from "./day3";
+import { resolvePart1, resolvePart2 } from "./day3";
 
 describe("Day 3", () => {
   it("should resolve the first demo", async () => {
@@ -8,5 +8,13 @@ describe("Day 3", () => {
 
   it("should resolve first problem", async () => {
     expect(await resolvePart1("day3.input.txt")).to.equal(525911);
+  });
+
+  it("should resolve the second demo", async () => {
+    expect(await resolvePart2("day3.input.demo.txt")).to.equal(467835);
+  });
+
+  it("should resolve the second problem", async () => {
+    expect(await resolvePart2("day3.input.txt")).to.equal(75805607);
   });
 });
--- 2023/day3/day3.ts
+++ 2023/day3/day3.ts
@@ -71,9 +71,10 @@ function isSymbol(element?: string) {
 export async function resolvePart2(filename: string) {
   const lines = await parseLines(filename, __dirname);
   
-  const partNumbers: Array<number> = [];
   let currentNumberString = "";
-  let currentNumberIsGear = false;
+  let currentStarIndex = null;
+
+  const gearMap: Record<string, number[]> = {}; // { X42Y67: [gearNumber, gearNumber] }}
   
   for (const rowIndex in lines) {
     const line = lines[rowIndex];
@@ -83,34 +84,41 @@ export async function resolvePart2(filename: string) {
 
       if (isDigit(element)) {
         currentNumberString += element;
-        if (!currentNumberIsGear && isGearNumber()) {
-          currentNumberIsGear = true;
+        
+        if (!currentStarIndex) {
+          currentStarIndex = getNearbyStarIndex();
         }
-      } else {
-        if (currentNumberIsGear) {
-          partNumbers.push(parseInt(currentNumberString));
-          currentNumberIsGear = false;
+
+      } else { // We've finished parsing the whole number
+        if (currentStarIndex) {
+          if (! gearMap[currentStarIndex]) { gearMap[currentStarIndex] = []; }
+
+          gearMap[currentStarIndex].push(parseInt(currentNumberString));
         }
-        currentNumberString = "";
+        currentNumberString = ""; // Then reset the currentNumberString
+        currentStarIndex = null;
       }
 
-      function isGearNumber() {
-        const previousLine = lines[parseInt(rowIndex) - 1];
+      function getNearbyStarIndex() {
+        const _rowIndex = parseInt(rowIndex);
+        const _columnIndex = parseInt(columnIndex);
+        const previousLine = lines[_rowIndex - 1];
         const currentLine = line;
-        const nextLine = lines[parseInt(rowIndex) + 1];
+        const nextLine = lines[_rowIndex + 1];
 
-        if (isStar(previousLine?.[parseInt(columnIndex) - 1]))  { return true; }
-        if (isStar(previousLine?.[parseInt(columnIndex) + 0]))  { return true; }
-        if (isStar(previousLine?.[parseInt(columnIndex) + 1]))  { return true; }
+        if (isStar(previousLine?.[_columnIndex - 1]))  {return `X${_rowIndex-1}Y${_columnIndex-1}`; }
+        if (isStar(previousLine?.[_columnIndex + 0]))  {return `X${_rowIndex-1}Y${_columnIndex+0}`; }
+        if (isStar(previousLine?.[_columnIndex + 1]))  {return `X${_rowIndex-1}Y${_columnIndex+1}`; }
 
-        if (isStar(currentLine[parseInt(columnIndex) - 1]))  { return true; }
-        if (isStar(currentLine[parseInt(columnIndex) + 0]))  { return true; }
-        if (isStar(currentLine[parseInt(columnIndex) + 1]))  { return true; }
+        if (isStar(currentLine[_columnIndex - 1]))  {return `X${_rowIndex-0}Y${_columnIndex-1}`; }
+        if (isStar(currentLine[_columnIndex + 0]))  {return `X${_rowIndex-0}Y${_columnIndex+0}`; }
+        if (isStar(currentLine[_columnIndex + 1]))  {return `X${_rowIndex-0}Y${_columnIndex+1}`; }
 
-        if (isStar(nextLine?.[parseInt(columnIndex) - 1]))  { return true; }
-        if (isStar(nextLine?.[parseInt(columnIndex) + 0]))  { return true; }
-        if (isStar(nextLine?.[parseInt(columnIndex) + 1]))  { return true; }
-        return false;
+        if (isStar(nextLine?.[_columnIndex - 1]))  {return `X${_rowIndex+1}Y${_columnIndex-1}`; }
+        if (isStar(nextLine?.[_columnIndex + 0]))  {return `X${_rowIndex+1}Y${_columnIndex+0}`; }
+        if (isStar(nextLine?.[_columnIndex + 1]))  {return `X${_rowIndex+1}Y${_columnIndex+1}`; }
+        
+        return null;
 
         function isStar(s: string) {
           return s === "*";
@@ -119,5 +127,10 @@ export async function resolvePart2(filename: string) {
     }
   }
 
-  return partNumbers.reduce((sum, nb) => sum + nb);
+  return Object
+    .values(gearMap)
+    .filter(gearNumbers => gearNumbers.length === 2)
+    .map(gearNumbers => gearNumbers[0] * gearNumbers[1])
+    .flat()
+    .reduce((sum, val) => sum + val);
 }
