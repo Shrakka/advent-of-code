3eeb48b9e34972d595b918eef1baa6715c8f44da
Enzo Testa <enzotesta@hotmail.fr>
2023-12-10 21:39:53 +0100

    day5 les lutins auront ma peau

--- 2023/day5/day5.test.ts
+++ 2023/day5/day5.test.ts
@@ -1,5 +1,40 @@
 import { expect } from "chai";
-import { resolveDemo1, resolveDemo2, resolvePart1, resolvePart2 } from "./day5";
+import { mergeRanges, resolveDemo1, resolveDemo2, resolvePart1, resolvePart2 } from "./day5";
+
+const allRanges = [
+  [
+    { minFrom: 50, maxFrom: 97, minTo: 52, maxTo: 99 },
+    { minFrom: 98, maxFrom: 99, minTo: 50, maxTo: 51 }
+  ],
+  [
+    { minFrom: 0, maxFrom: 14, minTo: 39, maxTo: 53 },
+    { minFrom: 15, maxFrom: 51, minTo: 0, maxTo: 36 },
+    { minFrom: 52, maxFrom: 53, minTo: 37, maxTo: 38 }
+  ],
+  [
+    { minFrom: 0, maxFrom: 6, minTo: 42, maxTo: 48 },
+    { minFrom: 7, maxFrom: 10, minTo: 57, maxTo: 60 },
+    { minFrom: 11, maxFrom: 52, minTo: 0, maxTo: 41 },
+    { minFrom: 53, maxFrom: 60, minTo: 49, maxTo: 56 }
+  ],
+  [
+    { minFrom: 18, maxFrom: 24, minTo: 88, maxTo: 94 },
+    { minFrom: 25, maxFrom: 94, minTo: 18, maxTo: 87 }
+  ],
+  [
+    { minFrom: 45, maxFrom: 63, minTo: 81, maxTo: 99 },
+    { minFrom: 64, maxFrom: 76, minTo: 68, maxTo: 80 },
+    { minFrom: 77, maxFrom: 99, minTo: 45, maxTo: 67 }
+  ],
+  [
+    { minFrom: 0, maxFrom: 68, minTo: 1, maxTo: 69 },
+    { minFrom: 69, maxFrom: 69, minTo: 0, maxTo: 0 }
+  ],
+  [
+    { minFrom: 56, maxFrom: 92, minTo: 60, maxTo: 96 },
+    { minFrom: 93, maxFrom: 96, minTo: 56, maxTo: 59 }
+  ]
+];
 
 describe("Day 5", () => {
   it("should solve the first demo", async () => {
@@ -10,11 +45,67 @@ describe("Day 5", () => {
     expect(resolvePart1()).to.equal(26273516);
   });
 
-  it.only("should solve the second demo", async () => {
+  it("should solve the second demo", async () => {
     expect(resolveDemo2()).to.equal(46);
   });
 
   it("should solve the second demo", async () => {
     expect(resolvePart2()).to.equal(0);
+  });
+
+  describe("merge ranges", () => {
+    it("should merge ranges", async () => {
+      const mergedRanges = mergeRanges(allRanges[0], allRanges[1]);
+      // [
+      //   { minFrom: 50, maxFrom: 97, minTo: 52, maxTo: 99 },
+      //   { minFrom: 98, maxFrom: 99, minTo: 50, maxTo: 51 }
+      // ],
+      // [
+      //   { minFrom: 0, maxFrom: 14, minTo: 39, maxTo: 53 },
+      //   { minFrom: 15, maxFrom: 51, minTo: 0, maxTo: 36 },
+      //   { minFrom: 52, maxFrom: 53, minTo: 37, maxTo: 38 }
+
+      expect(mergedRanges).to.deep.equal([
+        { minFrom: 0, maxFrom: 14, minTo: 39, maxTo: 53 },
+        { minFrom: 15, maxFrom: 49, minTo: 0, maxTo: 34 },
+        { minFrom: 50, maxFrom: 51, minTo: 37, maxTo: 38 },
+        { minFrom: 52, maxFrom: 97, minTo: 54, maxTo: 99 },
+        { minFrom: 98, maxFrom: 99, minTo: 35, maxTo: 36 }
+      ]);
+    });
+
+    it.only("should merge previously merged range with next range", async () => {
+      const mergedRanges = [
+        { minFrom: 0, maxFrom: 14, minTo: 39, maxTo: 53 },
+        { minFrom: 15, maxFrom: 49, minTo: 0, maxTo: 34 },
+        { minFrom: 50, maxFrom: 51, minTo: 37, maxTo: 38 },
+        { minFrom: 52, maxFrom: 97, minTo: 54, maxTo: 99 },
+        { minFrom: 98, maxFrom: 99, minTo: 35, maxTo: 36 }
+      ];
+
+      const nextRange = [
+        { minFrom: 0, maxFrom: 6, minTo: 42, maxTo: 48 },
+        { minFrom: 7, maxFrom: 10, minTo: 57, maxTo: 60 },
+        { minFrom: 11, maxFrom: 52, minTo: 0, maxTo: 41 },
+        { minFrom: 53, maxFrom: 60, minTo: 49, maxTo: 56 }
+      ]
+
+      expect(mergeRanges(mergedRanges, nextRange)).to.deep.equal([
+        { minFrom: 0, maxFrom: 6, minTo: 28, maxTo: 34 },
+        { minFrom: 7, maxFrom: 10, minTo: 35, maxTo: 38 },
+        { minFrom: 11, maxFrom: 14, minTo: 39, maxTo: 49 },
+        { minFrom: 15, maxFrom: 49, minTo: 42, maxTo: 23 },
+        { minFrom: 50, maxFrom: 51, minTo: 26, maxTo: 27 },
+        { minFrom: 52, maxFrom: 52, minTo: 50, maxTo: 50 },
+        { minFrom: 53, maxFrom: 97, minTo: 51, maxTo: 99 },
+        { minFrom: 98, maxFrom: 99, minTo: 24, maxTo: 25 }
+      ]); // VALEUR D'ARRIVER à vérifier !!
+    });
+
+    it.only("should recursively merge ranges", async () => {
+      const mergedRanges = allRanges.slice(0, 3).reduce((mergedRange, currentRange) => mergeRanges(mergedRange, currentRange));
+
+      console.log(mergedRanges);
+    });
   });
 });
--- 2023/day5/day5.ts
+++ 2023/day5/day5.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { log } from "node:console";
 import { parseLinesSync, range } from "../commons/utils";
 
@@ -8,6 +9,18 @@ function resolve1(filename: string) {
   return Math.min(
     ...seeds.map(seed => allMappings.reduce((seed, maps) => computeMappingOutput(seed, maps), seed))
   );
+
+  function computeMappingOutput(value: number, maps: Array<number[]>) {
+    let res = value;
+  
+    maps.forEach(([destination, source, length]) => {
+      if (value < source) { return; }
+      if (value > source + length) { return; }
+      res = value - source + destination;
+    });
+  
+    return res;
+  }
 }
 
 function parseSeedsAndMaps(lines: string[]) {
@@ -38,44 +51,155 @@ function parseSeedsAndMaps(lines: string[]) {
   return { seeds, allMappings };
 }
 
-function computeMappingOutput(value: number, maps: Array<number[]>) {
-  let res = value;
-
-  maps.forEach(([destination, source, length]) => {
-    if (value < source) { return; }
-    if (value > source + length) { return; }
-    res = value - source + destination;
-  });
-
-  return res;
-}
-
-
 function resolve2(filename: string) {
   const lines = parseLinesSync(filename, __dirname);
   const { seeds: seedsSettings, allMappings } = parseSeedsAndMaps(lines);
-
-  console.log({ seedsSettings });
   
-  console.log(
-    range(seedsSettings.length / 2).map(i => ({ min: seedsSettings[i * 2], max: seedsSettings[i * 2] + seedsSettings[i * 2 + 1]}))
-  )
+  const seedsRanges = range(seedsSettings.length / 2) 
+    .map(i => ({
+      min: seedsSettings[i * 2],
+      max: seedsSettings[i * 2] + seedsSettings[i * 2 + 1]}
+    ));
+  // seedsRanges = [ { min: 79, max: 93 }, { min: 55, max: 68 } ]
 
-  console.log("\n");
-  
+  const allRanges = allMappings.map(mappings => mappings.map(([destination, source, length]) => ({
+    minFrom: source,
+    maxFrom: source + length - 1,
+    minTo: destination,
+    maxTo: destination + length - 1
+  }))
+    .sort((m1, m2) => m1.minFrom - m2.minFrom));
 
-  log(
+  console.log(allRanges);
 
-    allMappings.map(mappings => mappings.map(([destination, source, length]) => ({
-      minFrom: source,
-      maxFrom: source + length - 1,
-      minTo: destination,
-      maxTo: destination+ length
-    })).sort((m1, m2) => m1.minFrom - m2.minFrom))
-  );
+  /* allRanges = [
+  [
+    { minFrom: 50, maxFrom: 97, minTo: 52, maxTo: 100 },
+    { minFrom: 98, maxFrom: 99, minTo: 50, maxTo: 52 }
+  ],
+  [
+    { minFrom: 0, maxFrom: 14, minTo: 39, maxTo: 54 },
+    { minFrom: 15, maxFrom: 51, minTo: 0, maxTo: 37 },
+    { minFrom: 52, maxFrom: 53, minTo: 37, maxTo: 39 }
+  ],
+  [
+    { minFrom: 0, maxFrom: 6, minTo: 42, maxTo: 49 },
+    { minFrom: 7, maxFrom: 10, minTo: 57, maxTo: 61 },
+    { minFrom: 11, maxFrom: 52, minTo: 0, maxTo: 42 },
+    { minFrom: 53, maxFrom: 60, minTo: 49, maxTo: 57 }
+  ]
+  */
 
+  // log(mergeRanges(allRanges[0], allRanges[1]))
+
+  const mergedRanges = allRanges.reduce((mergedRanges, currentRange) => mergeRanges(mergedRanges, currentRange));
+  console.log(mergedRanges);
 }
 
+export function mergeRanges(ranges1: Range[], ranges2: Range[]) {
+  /**
+  ranges1 = [
+    { minFrom: 50, maxFrom: 97, minTo: 52, maxTo: 100 },
+    { minFrom: 98, maxFrom: 99, minTo: 50, maxTo: 52 }
+  ]
+  ranges2 = [
+    { minFrom: 0, maxFrom: 14, minTo: 39, maxTo: 54 },
+    { minFrom: 15, maxFrom: 51, minTo: 0, maxTo: 37 },
+    { minFrom: 52, maxFrom: 53, minTo: 37, maxTo: 39 }
+  ],
+  */
+
+  // log({ ranges1, ranges2 });
+
+  const mergedRanges = _.unionBy([...ranges1, ...ranges2]
+    .sort((r1, r2) => {
+      if (r1.minFrom < r2.minFrom) { return -1; }
+      if (r1.minFrom > r2.minFrom) { return 1; }
+      return r1.maxFrom - r2.maxFrom;
+    }), r => r.minFrom);
+  
+  /*
+  mergedRanges = 
+    [
+      { minFrom: 0, maxFrom: 14, minTo: 39, maxTo: 54 },
+      { minFrom: 15, maxFrom: 51, minTo: 0, maxTo: 37 },
+      { minFrom: 50, maxFrom: 97, minTo: 52, maxTo: 100 },
+      { minFrom: 52, maxFrom: 53, minTo: 37, maxTo: 39 },
+      { minFrom: 98, maxFrom: 99, minTo: 50, maxTo: 52 }
+    ]
+  */
+
+  const resultRanges: Range[] = [];
+
+  for (let i = 0; i < mergedRanges.length; i++) {
+    const currentRange = mergedRanges[i];
+    const nextRange = mergedRanges[i + 1];
+
+    if (!nextRange) {
+      resultRanges.push({
+        minFrom: currentRange.minFrom,
+        maxFrom: currentRange.maxFrom,
+        minTo: -1,
+        maxTo: -1
+      });
+      continue;
+    }
+
+    if (currentRange.minFrom === nextRange.maxFrom) { continue; } // On prend le plus restreint
+
+    resultRanges.push({
+      minFrom: currentRange.minFrom,
+      maxFrom: nextRange.minFrom - 1,
+      minTo: -1,
+      maxTo: -1
+    });
+  }
+
+  resultRanges.forEach(ranges => {
+    ranges.minTo = traverseBothRanges(ranges.minFrom);
+    ranges.maxTo = traverseBothRanges(ranges.maxFrom);
+  });
+  /*
+   resultRanges = [
+    { minFrom: 0, maxFrom: 14, minTo: 39, maxTo: 53 },
+    { minFrom: 15, maxFrom: 49, minTo: 0, maxTo: 34 },
+    { minFrom: 50, maxFrom: 51, minTo: 37, maxTo: 38 },
+    { minFrom: 52, maxFrom: 97, minTo: 54, maxTo: 99 },
+    { minFrom: 98, maxFrom: 99, minTo: 35, maxTo: 36 }
+  ]
+  */
+  return resultRanges;
+  
+
+  function traverseBothRanges(value: number) { // value = 0
+    let firstRangeResult = value;
+
+    for (const range of ranges1) {
+      if (firstRangeResult < range.minFrom) { continue; }
+      if (firstRangeResult > range.maxFrom) { continue; }
+      firstRangeResult = firstRangeResult - range.minFrom + range.minTo; break;
+    }
+
+    let secondRangeResult = firstRangeResult;
+
+    for (const range of ranges2) {
+      if (secondRangeResult < range.minFrom) { continue; }
+      if (secondRangeResult > range.maxFrom) { continue; }
+      secondRangeResult = secondRangeResult - range.minFrom + range.minTo; break;
+    }
+
+    // console.log({ firstRangeResult, secondRangeResult });
+
+    return secondRangeResult;
+  }
+}
+
+interface Range {
+  minFrom: number;
+  maxFrom: number;
+  minTo: number;
+  maxTo: number;
+}
 
 
 export function resolveDemo1() {
